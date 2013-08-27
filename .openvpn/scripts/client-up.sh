d2d3c96fb5a4e1165b3d61d1f33508673ce57e4b
Barosl LEE <vcs@barosl.com>
2013-08-27 19:02:19 +0900

    .openvpn: Apply DNS servers pushed by the VPN server

---
+++ .openvpn/scripts/client-up.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+nameservers=
+searches=
+
+for key in ${!foreign_option_*}; do
+    val="${!key}"
+    arr=($val)
+    if [[ ${arr[0]} == 'dhcp-option' ]]; then
+        case ${arr[1]} in
+            DNS) nameservers+=" ${arr[2]}" ;;
+            DOMAIN) searches+=" ${arr[2]}" ;;
+        esac
+    fi
+done
+
+if [[ -n "$nameservers" || -n "$searches" ]]; then
+    if [[ ! -e /etc/resolv.conf.ovpn-ori ]]; then
+        if mv /etc/resolv.conf /etc/resolv.conf.ovpn-ori; then
+            for x in $nameservers; do
+                echo "nameserver $x" >> /etc/resolv.conf
+            done
+            for x in $searches; do
+                echo "search $x" >> /etc/resolv.conf
+            done
+        fi
+    fi
+fi
